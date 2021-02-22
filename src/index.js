5fe4d6aa89fd06bd6435d1d61cfbd3ea133c6813
Ali <ialimagomed@mail.ru>
2021-02-22 16:54:47 +0300

    fix: add my solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return !array || array.length == 0 ? 0 : Math.min(...array)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return !array || array.length == 0 ? 0 : Math.max(...array)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length == 0) return 0
+  let sum = 0
+  for (item of array) {
+    sum += item
+  }
+  return sum / array.length
 }
